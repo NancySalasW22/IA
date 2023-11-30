f866606ff51990bbef1a5ded26dbb388f4a94069
NancySalasW22 <106495604+NancySalasW22@users.noreply.github.com>
2023-11-29 22:05:38 -0600

    Add files via upload

---
+++ clasificadordeimagenes.py
@@ -0,0 +1,49 @@
+#NANCY JANETH PÉREZ SALAS
+#NANCY JANETH PÉREZ SALAS
+import numpy as np
+
+for imagenes_prueba, etiquetas_prueba in datos_pruebas.take(1):
+  imagenes_prueba = imagenes_prueba.numpy()
+  etiquetas_prueba = etiquetas_prueba.numpy()
+  predicciones = modelo.predict(imagenes_prueba)
+
+def graficar_imagen(i, arr_predicciones, etiquetas_reales, imagenes):
+  arr_predicciones, etiqueta_real, img = arr_predicciones[i], etiquetas_reales[i], imagenes[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img[...,0], cmap=plt.cm.binary)
+
+  etiqueta_prediccion = np.argmax(arr_predicciones)
+  if etiqueta_prediccion == etiqueta_real:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(nombres_clases[etiqueta_prediccion],
+                                100*np.max(arr_predicciones),
+                                nombres_clases[etiqueta_real]),
+                                color=color)
+
+def graficar_valor_arreglo(i, arr_predicciones, etiqueta_real):
+  arr_predicciones, etiqueta_real = arr_predicciones[i], etiqueta_real[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  grafica = plt.bar(range(10), arr_predicciones, color="#777777")
+  plt.ylim([0, 1])
+  etiqueta_prediccion = np.argmax(arr_predicciones)
+
+  grafica[etiqueta_prediccion].set_color('red')
+  grafica[etiqueta_real].set_color('blue')
+
+filas = 5
+columnas = 5
+num_imagenes = filas*columnas
+plt.figure(figsize=(2*2*columnas, 2*filas))
+for i in range(num_imagenes):
+  plt.subplot(filas, 2*columnas, 2*i+1)
+  graficar_imagen(i, predicciones, etiquetas_prueba, imagenes_prueba)
+  plt.subplot(filas, 2*columnas, 2*i+2)
+  graficar_valor_arreglo(i, predicciones, etiquetas_prueba)
